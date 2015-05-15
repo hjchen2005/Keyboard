7ffbace46db5a0697a1430c9aa6b181ac6349c84
hjchen2005 <hjchen2005@gmail.com>
2015-05-15 09:20:48 -0400

    Update scan_code_to_ascii.vhd

--- scan_code_to_ascii.vhd
+++ scan_code_to_ascii.vhd
@@ -1,66 +1,55 @@
-library ieee;
-use ieee.std_logic_1164.all;
-entity scan_code_to_ascii is
-port(
-shift : in std_logic;
-scan_code : in std_logic_vector(7 downto 0);
-ascii : out std_logic_vector(7 downto 0)
+Library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
+Entity scan_code_to_ascii is
+Port(
+scan_code : in STD_LOGIC_VECTOR(7 downto 0);
+shift : in STD_LOGIC;
+ascii : out STD_LOGIC_VECTOR(7 downto 0)
 );
-end scan_code_to_ascii;
-architecture arch of scan_code_to_ascii is
-begin
-process (scan_code, shift)
-begin
--- E
-if(scan_code = X"24" and shift = '0') then
-ascii <= X"65";
-elsif (scan_code = X"24" and shift = '1') then
-ascii <= X"45";
--- U
-elsif (scan_code = X"3C" and shift = '0') then
-ascii <= X"75";
-elsif (scan_code = X"3C" and shift = '1') then
-ascii <= X"55";
--- D
-elsif (scan_code = X"23" and shift = '0') then
-ascii <= X"64";
-elsif (scan_code = X"23" and shift = '1') then
-ascii <= X"44";
--- I
-elsif (scan_code = X"43" and shift = '0') then
-ascii <= X"69";
-elsif (scan_code = X"43" and shift = '1') then
-ascii <= X"49";
--- S
-elsif (scan_code = X"1B" and shift = '0') then
-ascii <= X"73";
-elsif (scan_code = X"1B" and shift = '1') then
-ascii <= X"53";
--- D
-elsif (scan_code = X"23" and shift = '0') then
-ascii <= X"64";
-elsif (scan_code = X"23" and shift = '1') then
-ascii <= X"44";
--- U
-elsif (scan_code = X"3C" and shift = '0') then
-ascii <= X"75";
-elsif (scan_code = X"3C" and shift = '1') then
-ascii <= X"55";
--- R
-elsif (scan_code = X"2D" and shift = '0') then
-ascii <= X"72";
-elsif (scan_code = X"2D" and shift = '1') then
-ascii <= X"52";
--- A
-elsif (scan_code = X"1C" and shift = '0') then
-ascii <= X"61";
-elsif (scan_code = X"1C" and shift = '1') then
-ascii <= X"41";
--- N
-elsif (scan_code = X"31" and shift = '0') then
-ascii <= X"6e";
-elsif (scan_code = X"31" and shift = '1') then
-ascii <= X"4e";
-end if;
-end process;
-end arch;
+End scan_code_to_ascii;
+Architecture arch of scan_code_to_ascii is
+Begin
+process(shift, scan_code)
+  begin
+	  --C
+		if (shift ='0' and scan_code = "01000011") then
+		  ascii <= "01000011";
+		--h
+		elsif (shift ='0' and scan_code = "01000011") then
+		  ascii <= "01101000";
+		 --e
+		elsif (shift ='0' and scan_code = "00101100") then
+		  ascii <= "01100101";
+	
+		  --n
+		elsif (shift ='0' and scan_code = "00110011") then
+		  ascii <= "01101110";
+		 --space
+	--H 
+	elsif (shift ='0' and scan_code = "01001000") then
+		  ascii <= "01001000";
+	--a
+	
+	elsif (shift ='0' and scan_code = "01100001") then
+		  ascii <= "01100001";
+	--n
+	
+	elsif (shift ='0' and scan_code = "01101110") then
+		  ascii <= "01101110";
+	--j
+	
+	elsif (shift ='0' and scan_code = "01101010") then
+		  ascii <= "01101010";
+	--u
+	
+	elsif (shift ='0' and scan_code = "01110101") then
+		  ascii <= "01110101";
+	--n
+	
+	elsif (shift ='0' and scan_code = "01101110") then
+		  ascii <= "01101110";
+	   end if;
+	end process;
+End arch;
+
